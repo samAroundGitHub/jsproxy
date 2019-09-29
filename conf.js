f9d22556976265f738dcbe0dc134d8afbd2c774d
Jōnouchi <hui379190004@icloud.com>
2019-09-29 21:04:26 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -40,7 +40,6 @@ jsproxy_config({
     // 该节点用于加载大体积的静态资源
     'cfworker': {
       label: '',
-      hidden: false,
       lines: {
         // 收费版（高权重）
         'node-cfworker-2.etherdream.com': 4,
@@ -58,7 +57,7 @@ jsproxy_config({
   /**
    * 默认节点
    */
-  node_default: 'mysite',
+  node_default: 'a',
   // node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
